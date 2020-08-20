0ef8530a84a086cfa7a86b427ed9da6f8c1f8c8f
rohanToss <rohantestrocky94@gmail.com>
2020-08-20 11:09:25 +0530

    changes in time made

--- controllers/controller-attendence.js
+++ controllers/controller-attendence.js
@@ -96,7 +96,7 @@ const moment = require('moment');
 			let currentTime = (todayTime[4]).split(':');
 			
 			// time = 10 am
-			if(currentTime[0] >= 10){
+			if(currentTime[0] > 10){
 				newAttendence.lateIn = true;
 				// check if late entry reason is provided or not
 				if(newAttendence.lateInReason==''){
