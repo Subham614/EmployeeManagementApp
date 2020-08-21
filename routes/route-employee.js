26b0d688b6bbe64c5f5c83fb5710446dca5c07b6
rohanToss <rohantestrocky94@gmail.com>
2020-08-21 17:41:24 +0530

    protecting route

--- routes/route-employee.js
+++ routes/route-employee.js
@@ -4,6 +4,6 @@ const auth = require('../auth/controller-auth');
 
 router.put('/api/password-update',auth.authenticateToken,controllerEmployee.updatePassword);
 router.get('/api/employees/:id',controllerEmployee.getEmployee);
-router.put('/api/update',controllerEmployee.updateEmployee);
+router.put('/api/update',auth.authenticateToken,controllerEmployee.updateEmployee);
 
 module.exports = router;
